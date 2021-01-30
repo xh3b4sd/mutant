7ff8c2b404659af4cc1ea69ecab7e81f70e79fb4
Tim Schindler <xh3b4sd@gmail.com>
2021-01-30 15:39:14 +0100

    add perm implementation

---
+++ pkg/perm/perm_test.go
@@ -0,0 +1,180 @@
+package perm
+
+import (
+	"reflect"
+	"strconv"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/xh3b4sd/mutant"
+)
+
+func Test_Perm_Lifecycle(t *testing.T) {
+	testCases := []struct {
+		shift func(p mutant.Interface)
+		index []int
+	}{
+		// Case 0 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+			},
+			index: []int{0, 0, 0},
+		},
+		// Case 1 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+			},
+			index: []int{0, 0, 1},
+		},
+		// Case 2 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+			},
+			index: []int{0, 1, 0},
+		},
+		// Case 3 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+			},
+			index: []int{0, 1, 1},
+		},
+		// Case 4 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+			},
+			index: []int{1, 0, 0},
+		},
+		// Case 5 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+			},
+			index: []int{1, 0, 1},
+		},
+		// Case 6 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+			},
+			index: []int{1, 1, 0},
+		},
+		// Case 7 ensures the permutation works as expected.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+			},
+			index: []int{1, 1, 1},
+		},
+		// Case 8 ensures mutating beyond capacity is idempotent.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+			},
+			index: []int{1, 1, 1},
+		},
+		// Case 9 ensures resetting starts over again.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+
+				p.Reset()
+			},
+			index: []int{0, 0, 0},
+		},
+		// Case 10 ensures resetting starts over again.
+		{
+			shift: func(p mutant.Interface) {
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+				p.Shift()
+
+				p.Reset()
+
+				p.Shift()
+			},
+			index: []int{0, 0, 1},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			var err error
+
+			var p mutant.Interface
+			{
+				c := Config{
+					Capacity: []int{1, 1, 1},
+				}
+
+				p, err = New(c)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			tc.shift(p)
+
+			index := p.Index()
+
+			if !reflect.DeepEqual(index, tc.index) {
+				t.Fatalf("\n\n%s\n", cmp.Diff(tc.index, index))
+			}
+		})
+	}
+}
